c925a8298d18db0bc52e9a339bab8cf293b4c6c6
Himanshu Parihar <86659009+Himanshu1600@users.noreply.github.com>
2022-06-24 17:53:29 +0530

    Create Binary Tree Maximum Path Sum.cpp

---
+++ Day_23/Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxsum(TreeNode* root,int &result){
+        if(root==NULL) return 0;
+        int left= max(0,maxsum(root->left,result));
+        int right= max(0,maxsum(root->right,result));
+        result=max(result,left+right+root->val);
+        return root->val+max(left,right);
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        int result=INT_MIN;
+        maxsum(root,result);
+        return result;
+    }
+};
