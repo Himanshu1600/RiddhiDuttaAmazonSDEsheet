d2c737114557cf99de0cba1bc1242f327995bb8e
Himanshu Parihar <86659009+Himanshu1600@users.noreply.github.com>
2022-07-05 12:32:40 +0530

    Create top view of binary tree.cpp

---
+++ Day_29/top view of binary tree.cpp
@@ -0,0 +1,25 @@
+vector<int> topView(Node *root)
+{
+    queue<pair<Node*,int>>q;
+    q.push({root,0});
+    map<int,int>mp;
+    vector<int>result;
+    while(!q.empty()){
+        Node* temp=q.front().first;
+        int level=q.front().second;
+        q.pop();
+        if(mp.find(level)==mp.end()){
+            mp[level]=temp->data;
+        }
+        if(temp->left){
+            q.push({temp->left,level-1});
+        }
+        if(temp->right){
+            q.push({temp->right,level+1});
+        }
+    }
+    for(auto it:mp){
+        result.push_back(it.second);
+    }
+    return result;
+}
